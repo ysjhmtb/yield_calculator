acebf3d77a52b49bfbd3e7e5753efbe39a4ff530
YSJ <ysjhmtb@gmail.com>
2018-05-01 03:23:13 +0900

    from the ground up

--- vix_test.py
+++ vix_test.py
@@ -1,36 +1,65 @@
 # -*- coding: utf-8 -*-
 
-import pandas as pd
+
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
 
 
-class dataInformation:
-    def findIndexOfDate(self, dfLength, dataframe, targetDate):
-        target_index = 0
-        for i in range(dfLength):
-            tempStr = str(dataframe.iloc[i:i + 1, 0:1])
-            if tempStr is not targetDate:
-                target_index = target_index + 1
-            else:
-                break
-        return target_index
+# 데이터 처리 클래스
+class dataProcessing:
+    def findIndexAtValue(self,raw_date,targetValue):
+        for i in range(len(raw_date)):
+            if raw_date[i] == targetValue:
+                return i
+    def strToFloat(self,raw_vix):
+        float_vix = []
+        for i in range(len(raw_vix)):
+            if raw_vix[i] != '.':
+                float_vix.append(float(raw_vix[i]))
+            elif raw_vix[i] == '.':
+                float_vix.append(0)
+        return float_vix
+
+proc = dataProcessing()
 
 
 # csv 파일 읽기
-rawdata = pd.read_csv('VIXCLS0414.csv')
-# print(rawdata.index)
-# print(rawdata.info())
+raw_date = []
+raw_vix = []
 
-# 2018-01-01 부터 출력
-print(rawdata.iloc[1225:, 0:2])
-raw_date = rawdata.iloc[1225:, 0:1]
-raw_vix = rawdata.iloc[1225:, 1:2]
+f = open('VIXCLS0414.csv', 'r', encoding='utf-8')
+rdr = csv.reader(f)
+for line in rdr:
+    raw_date.append(line[0])
+    raw_vix.append(line[1])
+f.close()
 
-plt.plot(raw_date,raw_vix)
-plt.gca().axes.get_xaxis().set_visible(False)
-plt.gca().axes.get_yaxis().set_visible(False)
-plt.show()
+
+if raw_date[0]=='DATE':
+    del raw_date[0]
+if raw_vix[0]=='VIXCLS':
+    del raw_vix[0]
+
+
+print(len(raw_date))    # 1304
+print(len(raw_vix))     # 1304
+
+print(proc.findIndexAtValue(raw_date,'2017-01-02')) # 965
+
+
+# raw_vix의 문자열을 실수로. '.' 예외처리 필요.
+raw_vix = proc.strToFloat(raw_vix)
+print(len(raw_vix))     # 1304
+
+
+# 60일 이동평균선 그리기
+
+
+
+
+
+
 
 # https://stackoverflow.com/questions/4805048/how-to-get-different-colored-lines-for-different-plots-in-a-single-figure
 
