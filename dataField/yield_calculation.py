2b37d985b29c8e9637669c50b49b825321164208
YSJ <ysjhmtb@gmail.com>
2018-07-23 15:40:49 +0900

    data updates

--- dataField/yield_calculation.py
+++ dataField/yield_calculation.py
@@ -33,12 +33,26 @@ kodex_leverage_list.append(RowObject("KODEX레버리지", "2017/12/26/TUE", "lon
 kodex_leverage_list.append(RowObject("KODEX레버리지", "2018/02/06/TUE", "long", 16415, 91))
 kodex_leverage_list.append(RowObject("KODEX레버리지", "2018/04/25/WED", "long", 16230, 123))
 
+kodex_leverage_list.append(RowObject("KODEX레버리지", "2018/06/18/MON", "long", 15330, 110))
+kodex_leverage_list.append(RowObject("KODEX레버리지", "2018/06/18/MON", "long", 15325, 110))
+kodex_leverage_list.append(RowObject("KODEX레버리지", "2018/06/18/MON", "long", 15315, 110))
+
 # KODEX 미국달러선물인버스2X
 
 
+# KODEX 미국달러선물레버리지
+dollar_leverage_list = []
+dollar_leverage_list.append(RowObject("KODEX미국달러선물레버리지", "2018/07/23/MON", "long", 8930, 100))
+dollar_leverage_list.append(RowObject("KODEX미국달러선물레버리지", "2018/07/23/MON", "long", 8935, 100))
+dollar_leverage_list.append(RowObject("KODEX미국달러선물레버리지", "2018/07/23/MON", "long", 8935, 20))
+
 # KODEX 레버리지 평균 매입 가격
 print(getAverageLongPrice(kodex_leverage_list))
-# 총 매수 자금 : 5777245
-# 16649.121037463978
+# 총 매수 자금 : 10833945
+# 16002.872968980797
 
+# KODEX 미국달러선물레버리지 평균 매입 가격
+print(getAverageLongPrice(dollar_leverage_list))
+# 총 매수 자금 : 1965200
+# 8932.727272727272
 
